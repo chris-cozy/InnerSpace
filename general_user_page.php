f65ff36019796d2d4d7b5d81f58e50170c80634b
chris-cozy <csande9@clemson.edu>
2021-11-21 12:06:48 -0500

    Fleshed out the general_user_page with content

--- general_user_page.php
+++ general_user_page.php
@@ -1,6 +1,11 @@
 <?php
 	session_start();
-	include_once 'connection.php'
+	include_once 'connection.php';
+    $creatorID = $_GET['creatorID'];
+	$creatorUser = $_GET['creatorUser'];
+    $_SESSION['creatorID'] = $creatorID;
+	$_SESSION['creatorUser'] = $creatorUser;
+    $uid = $_SESSION['userID'];
 ?>
 <!DOCTYPE html>
 <style>
@@ -51,43 +56,57 @@
     </head>
     <body style="background-color: rgb(42, 44, 44);">
         <header>
-            <h2 class="text"><a href="MeTube.html" class="text">MeTube<3</a></h2>
-            <h3 class="text">*User's name here*</h3>
+            <h2 class="text"><a href="MeTube.php" class="text">MeTube<3</a></h2>
+            <h3 class="text"><?php echo $_SESSION['creatorUser']; ?></h3>
         </header>
         <main>
             <section>
-				<div class="navbar">
-					<nav>
-						<ul class="text">
-							<li><a href="user_profile.html" ><b>Media</b></a></li>
-							<li><a href="./playlists.html" >Playlists</a></li>
-							<li><a href="./friends.html" >Friends</a></li>
-                            <li><a href="./about.html" >About</a></li>
-						</ul>
-					</nav>
-				</div>
+				<?php
+					$creatorID = $_GET['creatorID'];
+					$query = mysqli_query($conn, "SELECT * FROM account_info WHERE userID='$creatorID';") or die ("Query error".mysqli_error($conn)."\n");
+					$resultCheck = mysqli_num_rows($query);
+					if ($resultCheck > 0){
+						$row = mysqli_fetch_assoc($query);
+						if (isset($row['about_info'])){
+							$about = $row['about_info'];
+							$subs = $row['subscriber_count'];
+						}
+					}
+					echo "<h4 class='text'>ABOUT</h4>
+							<br>
+							<p>".$about."</p>
+							<br>
+							<h4 class='text'>Subscribers: ".$subs."</h4>";
+				?>
 			</section>
             <hr>
 			<!--Display recent uploaded medias-->
             <section>
-				<h2 class="text">Their Videos</h2>
+			<h2 class="text">Their Videos</h2>
 				<div>
 					<?php
 					$i = 0;
+					$creatorID = $_GET['creatorID'];
+					//WATCH PREPARED STATEMENTS VIDEO
 					$extensions_arr = array("mp4","avi","3gp","mov","mpeg");
-					$fetchVideos = mysqli_query($conn, "SELECT * FROM media WHERE userID = $_SESSION['userID'] ORDER BY mediaID DESC");
-					while($row = mysqli_fetch_assoc($fetchVideos) && $i < 4){
-						$type = $row['type'];
-						if(in_array($type,$extensions_arr)){
-							$location = $row['location'];
-							$name = $row['title'];
-							echo "<span style= 'display: inline-block;'>
-									<video src='".$location."' controls width='200px'>This video could not be displayed :/</video>
-									<br>
-									<span>".$name."</span>
-								</span>";
+					$fetchVideos = mysqli_query($conn, "SELECT * FROM media WHERE userID ='$creatorID' AND type = 'video' ORDER BY mediaID DESC;") or die ("Query error".mysqli_error($conn)."\n");
+
+					$resultCheck = mysqli_num_rows($fetchVideos);
+					if ($resultCheck > 0){
+						do{
+							$row = mysqli_fetch_assoc($fetchVideos);
+							if (isset($row['loc'])){
+								$location = $row['loc'];
+								$name = $row['title'];
+								echo "<span style= 'display: inline-block;'>
+										<video src='".$location."' controls width='200px'>This video could not be displayed :/</video>
+										<br>
+										<span><a href='../media_content.php?mediaID='".$row['mediaID']."''>".$name."</a></span>
+									</span>";
+							}
 							$i++;
-						}
+
+						}while($row && $i < 4 && $i < $resultCheck);
 					}
 					?>
 				</div>
@@ -99,21 +118,26 @@
 				<h2 class="text">Their Audio</h2>
 				<div>
 					<?php
+					$creatorID = $_GET['creatorID'];
 					$i = 0;
 					$extensions_arr = array("mp3");
-					$fetchVideos = mysqli_query($conn, "SELECT * FROM media WHERE userID = $_SESSION['userID'] ORDER BY mediaID DESC");
-					while($row = mysqli_fetch_assoc($fetchVideos) && $i < 4){
-						$type = $row['type'];
-						if(in_array($type,$extensions_arr)){
-							$location = $row['location'];
-							$name = $row['title'];
-							echo "<span style= 'display: inline-block;'>
-									<audio src='".$location."' controls type='audio/mpeg'>This audio could not be displayed :/</audio>
-									<br>
-									<span>".$name."</span>
-								</span>";
+					$fetchVideos = mysqli_query($conn, "SELECT * FROM media WHERE userID ='$creatorID' AND type = 'audio' ORDER BY mediaID DESC;") or die ("Query error".mysqli_error($conn)."\n");
+
+					$resultCheck = mysqli_num_rows($fetchVideos);
+					if ($resultCheck > 0){
+						do{
+							$row = mysqli_fetch_assoc($fetchVideos);
+							if (isset($row['loc'])){
+								$location = $row['loc'];
+								$name = $row['title'];
+								echo "<span style= 'display: inline-block;'>
+										<audio src='".$location."' controls type='audio/mpeg'>This audio could not be displayed :/</audio>
+										<br>
+										<span><a href='../media_content.php?mediaID='".$row['mediaID']."''>".$name."</a></span>
+									</span>";
+							}
 							$i++;
-						}
+						}while($row && $i < 4 && $i < $resultCheck);
 					}
 					?>
 				</div>
@@ -125,21 +149,28 @@
 				<h2 class="text">Their Images</h2>
 				<div>
 					<?php
+					$creatorID = $_GET['creatorID'];
 					$i = 0;
 					$extensions_arr = array("jpg","png");
-					$fetchVideos = mysqli_query($conn, "SELECT * FROM media WHERE userID = $_SESSION['userID'] ORDER BY mediaID DESC");
-					while($row = mysqli_fetch_assoc($fetchVideos) && $i < 4){
-						$type = $row['type'];
-						if(in_array($type,$extensions_arr)){
-							$location = $row['location'];
-							$name = $row['title'];
-							echo "<span style= 'display: inline-block;'>
-									<img src='".$location."' width='200' alt='This image could not be displayed :/'/>
-									<br>
-									<span>".$name."</span>
-								</span>";
+					$fetchVideos = mysqli_query($conn, "SELECT * FROM media WHERE userID ='$creatorID' AND type = 'image' ORDER BY mediaID DESC;") or die ("Query error".mysqli_error($conn)."\n");
+
+					$resultCheck = mysqli_num_rows($fetchVideos);
+
+					if ($resultCheck > 0){
+						do{
+							$row = mysqli_fetch_assoc($fetchVideos);
+							if (isset($row['loc'])){
+								$location = $row['loc'];
+								$name = $row['title'];
+								echo "<span style= 'display: inline-block;'>
+										<img src='".$location."' width='200' alt='This image could not be displayed :/'/>
+										<br>
+										<span><a href='../media_content.php?mediaID='".$row['mediaID']."''>".$name."</a></span>
+									</span>";
+							}
 							$i++;
-						}
+
+						}while($row && $i < 4 && $i < $resultCheck);
 					}
 					?>
 				</div>
