041eb038831d85bd95efa6ac58897c8e58f02f68
chris-cozy <csande9@clemson.edu>
2021-11-22 18:44:11 -0500

    Switched css to an external style sheet

--- profile/user_profile.php
+++ profile/user_profile.php
@@ -4,51 +4,11 @@
 	include 'functions.php';
 ?>
 <!DOCTYPE html>
-<style>
-    .text{
-		color: white;
-		font-family: monospace;
-		align-items: center;
-		text-decoration: none;
-	}
-	.logo{
-		color: white;
-		font-family: monospace;
-		font-size: 25px;
-		cursor: pointer;
-	}
-	.navbar{
-		width: 100%;
-		height: 15vh;
-		margin: auto;
-		display: flex;
-		align-items: center;
-	}
-	.headnav{
-		flex: 1;
-		padding-left: 100px;
-	}
-	nav ul li{
-		display: inline-block;
-		list-style: none;
-		margin: 0px 60px;
-	}
-	nav ul li a{
-		text-decoration: none;
-		color: rgb(255, 255, 255);
-		text-align: center;
-	}
-	.profile{
-		display: inline-block;
-		color: white;
-		margin: auto;
-		align-items: center;
-	}
-</style>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Metube: Profile</title>
+		<link rel="stylesheet" href="../styles.css">
     </head>
     <body style="background-color: rgb(42, 44, 44);">
         <header>
