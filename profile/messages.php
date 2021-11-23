4275e2d06514c442896d1d8feeb38eae932331dd
= <molive3@webapp1.computing.clemson.edu>
2021-11-22 20:58:08 -0500

    adding reply function

--- profile/messages.php
+++ profile/messages.php
@@ -56,11 +56,24 @@
 			$row2 = mysqli_fetch_assoc($senderresult);
 			$senderusername = $row2['username'];
 			$message = $row['message'];
+			$messageID = $row['messageID'];
 			echo "From: $senderusername\n";
 			echo "<br>";
 		        echo $message;
 			echo "<br>";
 			}
+			$query = "SELECT * FROM messages where messageID = '$messageID'";
+			$result =  mysqli_query($conn,$query) or die ("Query error".mysqli_error($conn)."\n");
+			$row2 = mysqli_fetch_assoc($result);
+			$replied = $row['reply'];
+			
+			if($replied == 1){
+				echo "You Replied: $reply_message";
+				echo "<br>";
+			}
+			else{
+				echo"<a href='messagereply.php?msgID='.$messageID.''> Reply</a>";
+			}	
 		}while($row);
 	
 	}
