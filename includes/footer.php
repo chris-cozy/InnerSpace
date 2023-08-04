3bc9b47436e3f15de279323c9e9889d7b5104f42
chris-cozy <csande9@clemson.edu>
2023-08-04 19:35:00 -0400

    Added header and footer files

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <section>
+        <div class="navbar">
+            <nav>
+                <ul class="text" style="margin: 0px 140px;">
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>FAQ</li>
+                </ul>
+            </nav>
+        </div>
+
+    </section>
+</footer>
