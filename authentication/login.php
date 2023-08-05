6dd50d99187d296f922c598d0deb72df350d85b3
chris-cozy <csande9@clemson.edu>
2023-08-04 23:06:32 -0400

    Implemented prepared statements, password verification, HTTP redirect status code

--- authentication/login.php
+++ authentication/login.php
@@ -1,61 +1,67 @@
 <?php
-  session_start();
-  include 'connection.php';
+include '../includes/session_manager.php';
+include '../includes/connection.php';
+
+SessionManager::startSession();
+
+function loginUser($username, $password, $conn)
+{
+  // Prepare the SQL statement with parameter binding
+  $query = "SELECT * FROM user_info WHERE username = ?";
+  $stmt = mysqli_prepare($conn, $query);
+  mysqli_stmt_bind_param($stmt, "s", $username);
+  mysqli_stmt_execute($stmt);
+
+  // Fetch the result
+  $result = mysqli_stmt_get_result($stmt);
+
+  if ($result && mysqli_num_rows($result) > 0) {
+    $user = mysqli_fetch_assoc($result);
+
+    // Verify the password using password_verify()
+    if (password_verify($password, $user['password'])) {
+      $_SESSION['userID'] = $user['userID'];
+      header("Location: MediaVerse.php", true, 302);
+      exit;
+    }
+  }
+  // If the login fails, display an error message
+  echo "Username or password incorrect";
+}
+
+// Check if the form is submitted
+if (isset($_POST['submit'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  loginUser($username, $password, $conn);
+}
 ?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
-  <title> Metube Login</title>
-  <link rel="stylesheet" href="styles.css">
+  <title>MediaVerse Login</title>
+  <link rel="stylesheet" href="../assets/css/styles.css">
 </head>
 
 <body>
   <p class='text'> Please login with your username and password</p>
 
-  <form action = "" method = "post" class='text'>
-      <label for "username">Username: </label><br>
-        <input type="text" id = "username" name = "username"><br>
+  <form action="" method="post" class='text'>
+    <label for "username">Username: </label><br>
+    <input type="text" id="username" name="username"><br>
 
-      <label for="password">Password: </label><br>
-        <input type="password" id="password" name="password"><br>
+    <label for="password">Password: </label><br>
+    <input type="password" id="password" name="password"><br>
 
-<input type="submit" value="Send" name="submit">
-<input type="reset">
+    <input type="submit" value="Send" name="submit">
+    <input type="reset">
 
-<p class='text'>Don't have an account? Sign up <a href='signup.php' class='text'>here</a></p>
+    <p class='text'>Don't have an account? Sign up <a href='signup.php' class='text'>here</a></p>
 
-</form>
+  </form>
 </body>
+
 </html>
-
-<?php
-  //checks if the user has submitted the information yet
-  if(isset($_POST['submit'])){
-
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-
-  $query = "SELECT *  from user_info where username='$username' and password = '$password'";
-  $result = mysqli_query($conn,$query) or die ("Query error".mysqli_error($conn)."\n");
-  $num_rows = mysqli_num_rows($result);
-
-  //if there is a username and password combo then it will take
-  //user to the homepage and have their account into
-  if($num_rows > 0){
-    //if the user exists, set the session to the user then go
-    //to homepage as a logged in user
-    $query = "SELECT userID from user_info where username ='$username'";
-    $queryres = mysqli_query($conn,$query) or die ("Query error".mysqli_error($conn)."\n");
-    $result = mysqli_fetch_array($queryres);
-    $_SESSION['userID'] = $result['userID'];
-    header("Location:MeTube.php");
-
-  }
-  //if not it will give error message
-  else{
-    echo "Username or password incorrect";
-  }
-}
-
-
-?>
