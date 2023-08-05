562fbd4d556ae332928ab74c3f16b788928c0189
chris-cozy <csande9@clemson.edu>
2023-08-05 10:44:23 -0400

    Implemented profile page

--- public/profile.php
+++ public/profile.php
@@ -2,3 +2,52 @@
 // This page will display the user's profile and allow them to update their profile information and profile picture.
 // Fetch the user's data from the database and display it.
 // Allow users to update their information through a form.
+
+// Include the database connection file
+require_once '../includes/db_connection.php';
+
+// Check if the user is logged in. Redirect to login page if not.
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit;
+}
+
+// Get the user ID from the session
+$user_id = $_SESSION['user_id'];
+
+try {
+    // Prepare a select statement to retrieve user data
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE user_id = :user_id");
+    $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+
+    // Execute the prepared statement
+    $stmt->execute();
+
+    // Fetch user data as an associative array
+    $user = $stmt->fetch();
+} catch (PDOException $e) {
+    // Handle any database errors
+    die("Error fetching user data: " . $e->getMessage());
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>User Profile</title>
+</head>
+
+<body>
+    <h2>User Profile</h2>
+    <p>Username: <?php echo $user['username']; ?></p>
+    <p>Email: <?php echo $user['email']; ?></p>
+    <p>Bio: <?php echo $user['bio']; ?></p>
+    <!-- Display user profile picture -->
+    <img src="<?php echo $user['profile_pic']; ?>" alt="Profile Picture" width="100" height="100">
+
+    <a href="update_profile.php">Update Profile</a>
+</body>
+
+</html>
