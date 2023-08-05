eea2acdceda3ea539ef574d0441275fa6ca95d26
chris-cozy <csande9@clemson.edu>
2023-08-05 09:21:08 -0400

    Protected against injection

--- profile/friends.php
+++ profile/friends.php
@@ -1,69 +1,80 @@
 <?php
-  session_start();
-  include "connection.php";
-  include 'functions.php';
-  ?>
+include '../includes/session_manager.php';
+include '../includes/connection.php';
+include '../includes/media_manager.php';
+include '../includes/user_auth.php';
+
+SessionManager::startSession();
+?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Metube: Profile</title>
-		<link rel="stylesheet" href="../styles.css">
-    </head>
-    <body>
-        <header>
-            <h2 class="text"><a href="../MeTube.php" class="text">MeTube<3</a></h2>
-            <h3 class="text"><?php 
-					$uid = $_SESSION['userID'];
-					getName($uid, $conn);
-				?></h3>
-        </header>
-        <main>
-            <section>
-				<div class="navbar">
-					<nav>
-						<ul class="text">
-							<li><a href="user_profile.php" >Media</a></li>
-							<li><a href="playlists.php" >Playlists</a></li>
-							<li><a href="friends.php" ><b>Friends</b></a></li>
-                            <li><a href="about.php" >About</a></li>
-							<li><a href="updateprofile.php">Update Profile</a></li>
-              				<li><a href="upload.php">Upload</a></li>
-                            <li><a href="messages.php" >Messages</a></li>
-						</ul>
-					</nav>
-				</div>
-			</section>
-            <hr>
-            <section>
-            	<?php
-                	//going through the database and getting the friends name
-                	$uid = $_SESSION['userID'];
-                	$query = "SELECT * from user_friends WHERE userID = '$uid'";
-                	$result = mysqli_query($conn,$query) or die ("Query error".mysqli_error($conn)."\n");
 
-                	$numrows = mysqli_num_rows($result);
+<head>
+	<meta charset="UTF-8">
+	<title>Friends</title>
+	<link rel="stylesheet" href="..assets/css/style.css">
+</head>
 
-                	if($numrows == 0){
-                  	echo "<p class='text'>You have no friends</p>";
-                	}
+<body>
+	<header>
+		<h2 class="text"><a href="../MeTube.php" class="text">MediaVerse</a>
+		</h2>
+		<h3 class="text"><?php
+							$uid = $_SESSION['userID'];
+							getName($uid, $conn);
+							?></h3>
+	</header>
+	<main>
+		<section>
+			<div class="navbar">
+				<nav>
+					<ul class="text">
+						<li><a href="user_profile.php">Media</a></li>
+						<li><a href="playlists.php">Playlists</a></li>
+						<li><a href="friends.php"><b>Friends</b></a></li>
+						<li><a href="about.php">About</a></li>
+						<li><a href="updateprofile.php">Update Profile</a></li>
+						<li><a href="upload.php">Upload</a></li>
+						<li><a href="messages.php">Messages</a></li>
+					</ul>
+				</nav>
+			</div>
+		</section>
+		<hr>
+		<section>
+			<?php
+			$uid = $_SESSION['userID'];
+			$query = "SELECT friendID FROM user_friends WHERE userID = ?";
+			$stmt = mysqli_prepare($conn, $query);
+			mysqli_stmt_bind_param($stmt, "i", $uid);
+			mysqli_stmt_execute($stmt);
+			$result = mysqli_stmt_get_result($stmt);
 
-                	else{
-                		//looping through all the friends for the user
-                		while($rows = mysqli_fetch_array($result)){
-                  		//using the join command to merge the table and get the information for the friends (username)
-                	  		$friendID = $rows['friendID'];
-                 	 		$query = "SELECT username from user_info WHERE userID = '$friendID'";
-                 	 		$result = mysqli_query($conn,$query) or die ("Query error".mysqli_error($conn)."\n");
-                 	 		$row = mysqli_fetch_assoc($result);
-							$friendName = $row['username'];
-				 	 		echo "<a href='../general_user_page.php?creatorID=".$friendID."&creatorUser=".$friendName."' class='text'>".$friendName."</a><br>";
-							  
-                		}
-              		}
-                ?>
-            </section>
-        </main>
-    </body>
+			$numrows = mysqli_num_rows($result);
+
+			if ($numrows == 0) {
+				echo "<p class='text'>You have no friends</p>";
+			} else {
+				while ($rows = mysqli_fetch_array($result)) {
+					$friendID = $rows['friendID'];
+					$query = "SELECT username FROM user_info WHERE userID = ?";
+					$stmt2 = mysqli_prepare($conn, $query);
+					mysqli_stmt_bind_param($stmt2, "i", $friendID);
+					mysqli_stmt_execute($stmt2);
+					$result2 = mysqli_stmt_get_result($stmt2);
+					$row = mysqli_fetch_assoc($result2);
+					$friendName = $row['username'];
+					echo "<a href='../general_user_page.php?creatorID=" . $friendID . "&creatorUser=" . $friendName . "' class='text'>" . $friendName . "</a><br>";
+				}
+			}
+
+			// Close the prepared statements
+			mysqli_stmt_close($stmt);
+			mysqli_stmt_close($stmt2);
+			?>
+		</section>
+	</main>
+</body>
+
 </html>
