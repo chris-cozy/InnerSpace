8b33d541b1322ecd8c42a0178cd270db7ebf7951
chris-cozy <csande9@clemson.edu>
2023-08-06 01:29:07 -0400

    Removed environment variable setting

--- public/index.php
+++ public/index.php
@@ -1,12 +1,6 @@
 <?php
 // This will be your app's landing page
 // You can include a welcome message or redirect users to the login or register page.
-require_once __DIR__ . '/vendor/autoload.php';
-
-// Load and parse .env file
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
 // Start the session to manage user authentication
 session_start();
 
