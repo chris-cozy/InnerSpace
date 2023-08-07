182feb8c26daf290cb712dd677a7bda5a3773f53
chris-cozy <csande9@clemson.edu>
2023-08-07 01:59:29 -0400

    Added styling to post details

--- public/post_details.php
+++ public/post_details.php
@@ -135,43 +135,93 @@ try {
 
 <head>
     <title>Post Details</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 
 <body>
-    <h2>Post Details</h2>
-    <div>
-        <p><?php echo $post['content']; ?></p>
-        <p>Posted by: <?php echo $post['username']; ?></p>
-        <?php if ($post['content_type'] === 'image') : ?>
-            <img src="<?php echo $post['media_path']; ?>" alt="Post Image" width="200">
-        <?php elseif ($post['content_type'] === 'video') : ?>
-            <video src="<?php echo $post['media_path']; ?>" controls width="200"></video>
-        <?php endif; ?>
-        <p>Like Count: <?php echo $like_count; ?></p>
-        <p>Dislike Count: <?php echo $dislike_count; ?></p>
-        <a href="profile.php?user_id=<?php echo $post['user_id']; ?>">View Profile</a>
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Connectify</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="home.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="explore.php">Explore</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="profile.php">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="post.php">Post</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="conversations.php">Messages</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container mt-5">
+        <div class="card mb-3">
+            <div class="card-body">
+                <a href="user_profile.php?user_id=<?php echo $post['user_id']; ?>" class="link-offset-2 link-underline link-underline-opacity-0">@<?php echo $post['username']; ?></a>
+                <p class="card-text"><?php echo $post['content']; ?></p>
+                <?php if ($post['content_type'] === 'image') : ?>
+                    <img src="<?php echo $post['media_path']; ?>" alt="Post Image" class="img-thumbnail" width="200">
+                <?php elseif ($post['content_type'] === 'video') : ?>
+                    <video src="<?php echo $post['media_path']; ?>" controls class="img-thumbnail" width="200"></video>
+                <?php endif; ?>
+
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="row">
+                <div class="col-md-4">
+                    <p class="card-text"><?php echo $like_count; ?> Likes</p>
+                </div>
+                <div class="col-md-4">
+                    <p class="card-text"><?php echo $dislike_count; ?> Dislikes</p>
+                </div>
+
+            </div>
+
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'] . '?post_id=' . $post_id); ?>" method="post" class="mt-3">
+                <input type="submit" name="like" value="Like" class="btn btn-primary">
+                <input type="submit" name="dislike" value="Dislike" class="btn btn-danger">
+            </form>
+        </div>
+
+        <hr>
+        <!-- Comment Form -->
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'] . '?post_id=' . $post_id); ?>" method="post">
-            <input type="submit" name="like" value="Like">
-            <input type="submit" name="dislike" value="Dislike">
+            <div class="mb-3">
+                <textarea class="form-control" id="comment_content" name="comment_content" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary btn-sm">Comment</button>
         </form>
+        <div class="container mt-4">
+            <ul class="list-group mb-3">
+                <?php foreach ($comments as $comment) : ?>
+                    <li class="list-group-item">
+                        <p class="mb-0">@<?php echo $comment['username']; ?> | <?php echo $comment['content']; ?></p>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+
     </div>
-
-    <h3>Comments:</h3>
-    <ul>
-        <?php foreach ($comments as $comment) : ?>
-            <li>
-                <p><?php echo $comment['content']; ?></p>
-                <p>Commented by: <?php echo $comment['username']; ?></p>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-
-    <!-- Comment Form -->
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'] . '?post_id=' . $post_id); ?>" method="post">
-        <label>Add a Comment:</label>
-        <textarea name="comment_content" required></textarea>
-        <input type="submit" name="comment" value="Post Comment">
-    </form>
+    <!-- Add Bootstrap JS (Popper.js and Bootstrap's JavaScript) -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
