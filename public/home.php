03ecb51f1f8bc1ddd7a28041676e3763f5375ae7
chris-cozy <csande9@clemson.edu>
2023-08-06 23:02:05 -0400

    Added styling to home page

--- public/home.php
+++ public/home.php
@@ -48,63 +48,94 @@ try {
 <html>
 
 <head>
-    <title>Home Page</title>
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 
 <body>
-    <h2>Home Page</h2>
-    <p>Welcome, <?php echo $user['username']; ?>!</p>
-    <p>Username: <?php echo $user['username']; ?></p>
-    <p>Email: <?php echo $user['email']; ?></p>
-    <p>Bio: <?php echo $user['bio']; ?></p>
-    <!-- Display user profile picture -->
-    <img src="<?php echo $user['profile_pic']; ?>" alt="Profile Picture" width="100" height="100">
-
-    <a href="update_profile.php">Update Profile</a>
-
-    <!-- Add navigation links -->
-    <p><a href="home.php">Home</a> | <a href="explore.php">Explore</a></p>
-
-    <h3>Posts from users you follow:</h3>
-    <?php
-    // Pagination settings
-    $posts_per_page = 5; // Change this number to control the number of posts per page
-    $total_posts = count($posts);
-    $total_pages = ceil($total_posts / $posts_per_page);
-
-    // Get the current page number from the URL
-    $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
-
-    // Calculate the starting index for the current page
-    $start_index = ($current_page - 1) * $posts_per_page;
-
-    // Get the posts for the current page
-    $current_page_posts = array_slice($posts, $start_index, $posts_per_page);
-    ?>
-    // Display the posts for the current page
-    <?php foreach ($current_page_posts as $post) :
-    ?>
-        <div>
-            <p><?php echo $post['content']; ?></p>
-            <p>Posted by: <?php echo $post['username']; ?></p>
-            <?php if ($post['content_type'] === 'image') : ?>
-                <img src="<?php echo $post['media_path']; ?>" alt="Post Image" width="200">
-            <?php elseif ($post['content_type'] === 'video') : ?>
-                <video src="<?php echo $post['media_path']; ?>" controls width="200"></video>
-            <?php endif; ?>
-            <!-- Add link to post details page -->
-            <a href="post_details.php?post_id=<?php echo $post['post_id']; ?>">View Details</a>
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Connectify</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="home.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="explore.php">Explore</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="profile.php">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="post.php">Post</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="conversations.php">Messages</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    <?php endforeach; ?>
+    </nav>
+    <div class="container mt-5">
+        <h2 class="mb-4">Home</h2>
+        <hr>
 
-    <!-- Pagination links -->
-    <?php for ($i = 1; $i <= $total_pages; $i++) : ?>
-        <?php if ($i === $current_page) : ?>
-            <strong><?php echo $i; ?></strong>
-        <?php else : ?>
-            <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
-        <?php endif; ?>
-    <?php endfor; ?>
+
+        <?php
+        // Pagination settings
+        $posts_per_page = 5; // Change this number to control the number of posts per page
+        $total_posts = count($posts);
+        $total_pages = ceil($total_posts / $posts_per_page);
+
+        // Get the current page number from the URL
+        $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+        // Calculate the starting index for the current page
+        $start_index = ($current_page - 1) * $posts_per_page;
+
+        // Get the posts for the current page
+        $current_page_posts = array_slice($posts, $start_index, $posts_per_page);
+        ?>
+        <?php foreach ($current_page_posts as $post) :
+        ?>
+            <div class="card mb-3">
+                <div class="card-body">
+                    <p class="card-text"><?php echo $post['content']; ?></p>
+                    <p class="card-text">Posted by: <?php echo $post['username']; ?></p>
+                    <?php if ($post['content_type'] === 'image') : ?>
+                        <img src="<?php echo $post['media_path']; ?>" alt="Post Image" class="img-thumbnail" width="200">
+                    <?php elseif ($post['content_type'] === 'video') : ?>
+                        <video src="<?php echo $post['media_path']; ?>" controls class="img-thumbnail" width="200"></video>
+                    <?php endif; ?>
+                    <!-- Add link to post details page -->
+                    <a href="post_details.php?post_id=<?php echo $post['post_id']; ?>" class="btn btn-primary btn-sm">View Details</a>
+                </div>
+            </div>
+        <?php endforeach; ?>
+
+        <!-- Pagination links -->
+        <nav aria-label="Page navigation">
+            <ul class="pagination">
+                <?php for ($i = 1; $i <= $total_pages; $i++) : ?>
+                    <?php if ($i === $current_page) : ?>
+                        <li class="page-item active" aria-current="page">
+                            <span class="page-link"><?php echo $i; ?><span class="sr-only">(current)</span></span>
+                        </li>
+                    <?php else : ?>
+                        <li class="page-item"><a class="page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+                    <?php endif; ?>
+                <?php endfor; ?>
+            </ul>
+        </nav>
+    </div>
+    <!-- Add Bootstrap JS (Popper.js and Bootstrap's JavaScript) -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
